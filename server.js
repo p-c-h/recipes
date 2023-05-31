4a0111c0b494c78bbbab7590624108af62d32a23
p-c-h <105007901+Pi-code246@users.noreply.github.com>
2023-05-31 22:26:38 +0200

    add express.static middleware, add /find route handler

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.set("view engine", "ejs");
 
 app.listen(5050);
 
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
@@ -41,7 +43,45 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+app.post("/find", (req, res) => {
+  function findMatchingRecipes(ingredients, recipes) {
+    const matchedRecipes = [];
+
+    for (let i = 0; i < recipes.length; i++) {
+      const recipe = recipes[i];
+      let matchedCount = 0;
+
+      for (let j = 0; j < recipe.ingredients.length; j++) {
+        if (ingredients.includes(recipe.ingredients[j])) {
+          matchedCount++;
+        }
+      }
+
+      if (matchedCount > 0) {
+        matchedRecipes.push({
+          name: recipe.name,
+          ingredients: recipe.ingredients,
+          matchedCount,
+        });
+      }
+    }
+
+    matchedRecipes.sort((a, b) => b.matchedCount - a.matchedCount);
+
+    return matchedRecipes;
+  }
+
+  fs.readFile("./data/recipes.json", function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+    if (data.length > 0) {
+      const recipes = JSON.parse(data);
+      const ingredients = req.body["inStock[]"];
+      const matchedRecipes = findMatchingRecipes(ingredients, recipes);
+      res.render("index", matchedRecipes);
+    } else {
+      res.end();
+    }
+  });
 });
